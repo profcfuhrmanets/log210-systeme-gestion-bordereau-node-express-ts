c9881939ea34f28735c9b54fcd304a3cbfddc5d1
Olivier Brochu <olivier_brochu@hotmail.com>
2024-06-10 14:35:53 +0200

    Corriger les fautes dans les tests aussi

--- tests/controller/CourseController.test.ts
+++ tests/controller/CourseController.test.ts
@@ -6,7 +6,7 @@ describe('CourseControllerTest', () => {
   it('get all courses', () => {
     let controller: CourseController = new CourseController();
     let courses:CourseJSON[] = controller.all();
-    expect(courses).toEqual( [{"id": "LOG121",  "prealable": "none", "titre": "Conception orientée object"}, {"id": "LOG210", "prealable": "LOG121", "titre": "Analyse et conception de logiciels"}, {"id": "LOG240", "titre": "Test et maintenance"}, {"id": "LOG320", "prealable": "LOG121", "titre": "Structures de données et algorithmes"}, {"id": "LOG410", "prealable": "LOG240", "titre": "AAnalyse de besoins et spécifications"}, {"id": "LOG430", "prealable": "LOG210", "titre": "Architecture logicielle"}])
+    expect(courses).toEqual( [{"id": "LOG121",  "prealable": "none", "titre": "Conception orientée objet"}, {"id": "LOG210", "prealable": "LOG121", "titre": "Analyse et conception de logiciels"}, {"id": "LOG240", "titre": "Tests et maintenance"}, {"id": "LOG320", "prealable": "LOG121", "titre": "Structures de données et algorithmes"}, {"id": "LOG410", "prealable": "LOG240", "titre": "Analyse de besoins et spécifications"}, {"id": "LOG430", "prealable": "LOG210", "titre": "Architecture logicielle"}])
   })
     
 });
